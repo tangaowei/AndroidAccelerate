64dd2cc89164bce21581aadc82e556ddf4c239d3
tangaowei <tangaowei@jd.com>
2021-09-30 19:44:05 +0800

    feat:添加plugin demo

--- ArchDemo/app/src/main/java/com/tgw/arch/viewmodel/ExhibitViewModel.kt
+++ ArchDemo/app/src/main/java/com/tgw/arch/viewmodel/ExhibitViewModel.kt
@@ -5,21 +5,24 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tgw.arch.data.VisitorBean
 import com.tgw.arch.retrofit.RetrofitClient
+import hugo.weaving.DebugLog
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class ExhibitViewModel : ViewModel() {
     var visitorBean : MutableLiveData<VisitorBean> = MutableLiveData()
+
+//    @DebugLog
     fun requestVisitorRetrofit(): MutableLiveData<VisitorBean> {
         RetrofitClient().apiService().getVisitor().enqueue(
             object : Callback<VisitorBean> {
                 override fun onFailure(call: Call<VisitorBean>, t: Throwable) {
-                    Log.d("TGW", "----errorMsg: {$call}")
+//                    Log.d("TGW", "----errorMsg: {$call}")
                 }
 
                 override fun onResponse(call: Call<VisitorBean>, response: Response<VisitorBean>) {
-                    Log.d("TGW", "----response.body(): {${response.body()}}")
+//                    Log.d("TGW", "----response.body(): {${response.body()}}")
                     visitorBean.value = response.body()
                 }
 
